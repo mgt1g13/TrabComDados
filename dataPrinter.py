1597c1ce8cbc20caa8c3d5c209146a00409a5b01
GuiGaiardo <guigaiardo@gmail.com>
2015-06-05 19:28:16 -0300

    classe que pega os dados e grava no arquivo de texto dOut.txt

---
+++ dataPrinter.py
@@ -0,0 +1,15 @@
+﻿import sys
+from bitstring import BitArray
+
+
+class dataPrinter:
+
+    def __init__(self, fileName):
+        self.outputDataFile = open(fileName, 'r+')
+        print("Arquivo aberto, começando a escrever.")
+
+    def printData(self, pData):
+        self.outputDataFile.write(pData.to_bytes((pData.bit_length()+7)//8, 'big').decode())
+
+    def closeFile(self):
+        self.outputDataFile.close()
