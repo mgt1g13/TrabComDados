db46371b60bd8c185091870ad1377e2289942943
GuiGaiardo <guigaiardo@gmail.com>
2015-06-07 19:48:27 -0300

    Agora funciona com o frameMaker

--- senderTest.py
+++ senderTest.py
@@ -1,17 +1,13 @@
 import socket
 from dataGenerator import dataGenerator
 from bitstring import BitArray
-
+from frameMaker import frameMaker
 
 class Sender:
     
-    def __init__(self, host):
-        self.dest = (host, 5000)
-        __initSocket()
-
     def __init__(self, host, port):
         self.dest = (host, port)
-        __initSocket()
+        self.__initSocket()
 
     def __initSocket(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -20,21 +16,17 @@ class Sender:
         self._socket.close()
 
     def send(self, frame):
-        self._socket.sendto(bytes(frame.bin, 'utf-8'), destino)
+        self._socket.sendto(bytes(frame.bin, 'utf-8'), self.dest)
         
           
 
 HOST = '127.0.0.1'
 PORT = 5000
-sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-destino = (HOST, PORT)
 
-file = "dados.txt"
-dataGen = dataGenerator(file)
+sender = Sender(HOST, PORT)
+frame = frameMaker()
 
-i=0
-while i<50:
-    frame = dataGen.getData()
-    sender.sendto(bytes(frame.bin, 'utf-8'), destino)
-    i = i+1
-sender.close()
+for i in range(0,49):
+    sender.send(frame.getFrame())
+    
+sender.closeSocket()
