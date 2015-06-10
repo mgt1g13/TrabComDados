7e8b61e202704d6dc83bb283886210acf8840857
GuiGaiardo <guigaiardo@gmail.com>
2015-06-10 20:59:54 -0300

    Alteracoes para funcionar com metadados tambem

--- frameDecoder.py
+++ frameDecoder.py
@@ -6,6 +6,8 @@
 from dataPrinter import dataPrinter
 from flagger import Flagger
 from bitstring import BitArray
+from controlData import controlData
+from checksum import Checksummer
 
 
 
@@ -13,14 +15,28 @@ class frameDecoder:
 
 
     def __init__(self, frameSize = 5, OUTfileName = 'dOut.txt', flag = BitArray('0b00101110')):
-        self.nFrame = 0
-
         self.dataPrinter = dataPrinter(OUTfileName)
+        self.controlData = controlData(1, 0)
+        self.checksummer = Checksummer()
         self.flagger = Flagger(flag)
+        self.nFrame = 0
         
 
     def decodeFrame(self, frame):
+        #retira a flag
         frame = self.flagger.decode(frame)
+
+        #checa e retira o checksum
+        frame = self.checksummer.verifyChecksum(frame)
+        #if (frame == -1):
+        #    print("checksum detectou um erro")
+
+        #retorna os dados de controle
+        controlData = self.controlData.getControlData(frame)
+        #trata os dados de controle
+        
+        frame = frame[20:]
+        print (frame.bin)
         data = int(frame.bin, 2)
         self.dataPrinter.printData(data)
 
