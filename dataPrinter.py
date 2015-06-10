c71335c6512e48f3d8e70f0d71dc23034d017aca
GuiGaiardo <guigaiardo@gmail.com>
2015-06-09 21:56:40 -0300

    opcao do open setada para 'a+'

--- dataPrinter.py
+++ dataPrinter.py
@@ -5,7 +5,7 @@ from bitstring import BitArray
 class dataPrinter:
 
     def __init__(self, fileName):
-        self.outputDataFile = open(fileName, 'r+')
+        self.outputDataFile = open(fileName, 'a+')
         print("Arquivo aberto, começando a escrever.")
 
     def printData(self, pData):
