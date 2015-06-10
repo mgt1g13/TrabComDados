b2851ccaf43be2afbb449f6bbe6103925d5f0d01
mgt1g13 <mgt1g13@soton.ac.uk>
2015-06-10 01:14:43 -0300

    Checksum
    Adiciona classe para fazer o Checksum

---
+++ checksum.py
@@ -0,0 +1,45 @@
+from bitstring import BitArray
+
+class Checksummer:
+
+    def __init__(self, checksumSize = 8):
+        self.checksumSize = checksumSize
+        return
+
+    def addChecksum(self, frame):
+        frame.append(self._calculateChecksum(frame))
+        return frame
+
+    #Retorna o quadro sem o checksum se ele está correto
+    #Caso contrário retorna -1
+    def verifyChecksum(self, frame):
+        checksumGiven = frame[(len(frame) - self.checksumSize):]
+        data = frame[:(len(frame) - self.checksumSize)]
+        checksumCalculated = self._calculateChecksum(data)
+
+        if(checksumGiven == checksumCalculated):
+            return data
+        else:
+            return -1
+        
+            
+    def _calculateChecksum(self, data):
+        i = 0
+        checksum = self.checksumSize*BitArray('0b0') #checksumSize bits 0
+        #Vai pegando de 8 em 8 bits e fazendo o xor para calcular o checksum
+        while(i < len(data)):
+            if(len(checksum) == len(data[i:i+self.checksumSize])): 
+                checksum = checksum ^ data[i:i+self.checksumSize]
+            else:
+                #Coloca 0s no que remaneceu do quadro para calcular o checksum
+                temp = data[i:i+self.checksumSize]
+                temp.prepend((len(checksum)-len(data[i:i+self.checksumSize]))*BitArray('0b0'))
+                checksum = checksum ^ (temp)
+            i = i + self.checksumSize
+        return checksum
+
+
+c = Checksummer()
+a = BitArray('0b001100111')
+c.addChecksum(a)
+print(c.verifyChecksum(a).bin)
