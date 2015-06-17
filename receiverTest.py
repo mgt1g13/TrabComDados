0ff5d5e0dc5e4a7f59f74fd0c41c04d8b3196d39
GuiGaiardo <guigaiardo@gmail.com>
2015-06-17 19:04:49 -0300

    Funcionando sem o meio!

--- receiverTest.py
+++ receiverTest.py
@@ -1,31 +1,46 @@
 import socket
 from bitstring import BitArray
 from frameDecoder import frameDecoder
+from DataLinkControl import DataLinkReceiverControl
 
 class Receiver:
     
-    def __init__(self, host, port = 5000):
+    def __init__(self, host, source = '127.0.0.1', port = 5000):
         self.origem = (host, port)
+        self.ackDest = (source, (port+1))
         self.__initSocket()
 
     def __initSocket(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._socket.bind(self.origem)
+        self._ackSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def closeSocket(self):
         self._socket.close()
+        self._ackSocket.close()
 
     def receive(self):
-        dataIn, cliente = self._socket.recvfrom(1024)
+        dataIn, client = self._socket.recvfrom(1024)
         return BitArray('0b'+dataIn.decode('utf-8'))
+
+    def sendAck(self, frame):
+        self._ackSocket.sendto(bytes(frame.bin, 'utf-8'), self.ackDest)
+
+
+
 
 
 
 rec = Receiver('')
-decoder = frameDecoder()
+dc = DataLinkReceiverControl()
 
-for x in range(0,49):
-    decoder.decodeFrame(rec.receive())
-    
-decoder.endSession()
+x = 0
+while True:
+    nextFrame = rec.receive()
+    if nextFrame.bin == '0':
+        break
+    rec.sendAck(dc.receiveFrame(nextFrame))
+    print("It: ", x)
+    x = x+1
+dc.endSession()
 rec.closeSocket()
--- senderTest.py
+++ senderTest.py
@@ -1,22 +1,37 @@
 import socket
-from dataGenerator import dataGenerator
+from DataLinkControl import DataLinkSenderControl
 from bitstring import BitArray
 from frameMaker import frameMaker
 
 class Sender:
     
-    def __init__(self, host, port):
+    def __init__(self, host = '127.0.0.1', port = 5000):
         self.dest = (host, port)
+        self.ackSrc = ('', (port+1))
         self.__initSocket()
 
     def __initSocket(self):
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._ackSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._ackSocket.bind(self.ackSrc)
+        self._ackSocket.setblocking(0)
 
     def closeSocket(self):
         self._socket.close()
+        self._ackSocket.close()
 
     def send(self, frame):
         self._socket.sendto(bytes(frame.bin, 'utf-8'), self.dest)
+
+    def receiveAck(self):
+        try:
+            ackFrame, client = self._ackSocket.recvfrom(1024)
+        except socket.error:
+            return None
+        else:
+            ack = BitArray('0b' + ackFrame.decode('utf-8'))
+            print ("Ack bin: ", ack.bin)
+            return ack
         
           
 
@@ -24,9 +39,18 @@ HOST = '127.0.0.1'
 PORT = 5000
 
 sender = Sender(HOST, PORT)
-frame = frameMaker()
+dc = DataLinkSenderControl()
 
-for i in range(0,49):
-    sender.send(frame.getFrame())
-    
+x = 0
+while True:
+    ack = sender.receiveAck()
+    if ack != None:
+        if dc.validateAck(ack) == 1:
+            sender.send(BitArray('0b0'))
+            break
+    nextFrame = dc.getFrame()
+    if nextFrame != None:
+        sender.send(nextFrame)    
+    print ("It: ", x)
+    x = x+1
 sender.closeSocket()
